cfebafa0665ecfdc4be0176e0b60a064f60b4d9b
Mohammed Saif <isaeefullah@gmail.com>
2020-07-24 22:35:19 +0530

    added intial loading screen

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+  
+<div class="boxes">
+    <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    <div class="box">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+  );
+}
+
+export default App;
